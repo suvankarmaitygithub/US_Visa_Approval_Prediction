7999b8e52e687398f8def8e450db338f07a50a22
Suvankar <suvankarmaitycollege@gmail.com>
2024-02-23 22:10:44 +0530

    updated

---
+++ demo.py
@@ -0,0 +1,11 @@
+from US_Visa.logger import logging
+
+
+from US_Visa.exception import USvisaException
+import sys
+
+try:
+    a = 1 / "10"
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e, sys) from e
